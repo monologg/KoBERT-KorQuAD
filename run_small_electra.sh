ae1f5ac67896a892991296bfd753956d0cd70a09
monologg <adieujw@gmail.com>
2020-04-19 00:31:15 +0900

    Change hyperparam

--- run_small_electra.sh
+++ run_small_electra.sh
@@ -6,9 +6,9 @@ python3 run_squad.py --model_type koelectra-small \
                        --train_file KorQuAD_v1.0_train.json \
                        --predict_file KorQuAD_v1.0_dev.json \
                        --evaluate_during_training \
-                       --per_gpu_train_batch_size 8 \
-                       --per_gpu_eval_batch_size 8 \
+                       --per_gpu_train_batch_size 32 \
+                       --per_gpu_eval_batch_size 64 \
                        --max_seq_length 512 \
-                       --logging_steps 4000 \
-                       --save_steps 4000 \
+                       --logging_steps 1000 \
+                       --save_steps 1000 \
                        --do_train
